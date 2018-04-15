a9c1e3c2a5ac7d07e19f48ceec23c45c5d77a23a
iamdanialkamali <daniel.kamali@yahoo.com>
2018-04-15 23:04:01 +0430

    Initial commit

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,33 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from accounts import views as accounts_views
+from boards import views
+
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/',views.home,name ='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('boards/<int:pk>/',views.board_topics,name='board_topics'),
+    path('boards/<int:pk>/new/',views.new_topic,name='new_topic'),
+    path('signup/', accounts_views.signup, name='signup'),
+]
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\iamdanialkamali\myproject\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
